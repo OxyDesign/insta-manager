bffb161f5a0ff3abd825d5356210c9c7edcf28a6
Nicolas Escoffier <nico.oxydesign@free.fr>
2015-10-16 01:10:10 +0100

    Adding Angular

--- src/js/instaManager.js
+++ src/js/instaManager.js
@@ -1,1 +1,5 @@
-console.log('instaManager');
+var instaManager = angular.module('instaManager',[]);
+
+instaManager.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log('mainCtrl');
+}]);
