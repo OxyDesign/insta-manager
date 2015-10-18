d3c0f0c7302637f964b9380230495baba4a07326
Nicolas Escoffier <nico.oxydesign@free.fr>
2015-10-18 22:36:21 +0200

    Dummy Data + Angular Loop

--- src/js/instaManager.js
+++ src/js/instaManager.js
@@ -1,5 +1,25 @@
+
 var instaManager = angular.module('instaManager',[]);
 
 instaManager.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log('mainCtrl');
+    var dummyData = {
+        "data":[
+            {"username":"UserName1","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 1"},
+            {"username":"UserName2","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 2"},
+            {"username":"UserName3","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 3"},
+            {"username":"UserName4","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 4"},
+            {"username":"UserName5","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 5"}
+        ]
+    };
+    var count = 5;
+
+    $scope.load = function(){
+        if(!$scope.data){
+            $scope.data = dummyData;
+        }else{
+            $scope.data.data = $scope.data.data.concat(Array.apply(null, {length: 5}).map(function(){
+                return {"username":"UserName"+(++count),"profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name "+count};
+            }));
+        }
+    };
 }]);
