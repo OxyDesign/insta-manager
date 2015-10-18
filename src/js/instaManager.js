d8c6a9af4d645a5ff5adfcfa8a8d39f17731692d
Nicolas Escoffier <nico.oxydesign@free.fr>
2015-10-19 00:44:26 +0100

    API Instagram + Ajax Call + Binding id & token

--- src/js/instaManager.js
+++ src/js/instaManager.js
@@ -2,24 +2,28 @@
 var instaManager = angular.module('instaManager',[]);
 
 instaManager.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
-    var dummyData = {
-        "data":[
-            {"username":"UserName1","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 1"},
-            {"username":"UserName2","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 2"},
-            {"username":"UserName3","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 3"},
-            {"username":"UserName4","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 4"},
-            {"username":"UserName5","profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name 5"}
-        ]
-    };
-    var count = 5;
+    var url;
+    $scope.lastPage = false;
+
+    $scope.token = '';
+    $scope.id = '';
 
     $scope.load = function(){
+        if($scope.lastPage || !$scope.token || !$scope.id) return;
+
         if(!$scope.data){
-            $scope.data = dummyData;
-        }else{
-            $scope.data.data = $scope.data.data.concat(Array.apply(null, {length: 5}).map(function(){
-                return {"username":"UserName"+(++count),"profile_picture":"pictureUrl","id":"000000000","full_name":"Full Name "+count};
-            }));
+            $scope.data = [];
+            url = 'https://api.instagram.com/v1/users/'+$scope.id+'/followed-by?access_token='+$scope.token;
         }
+
+        $http.jsonp(
+            url+'&callback=JSON_CALLBACK'
+        ).then(function (response) {
+            $scope.data = $scope.data.concat(response.data.data);
+            url = response.data.pagination.next_url;
+            if(!url) $scope.lastPage = true;
+        }, function (response) {
+            console.log('Error : ',response);
+        });
     };
 }]);
